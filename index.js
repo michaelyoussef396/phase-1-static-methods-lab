5419668232267bc6725040eaf697c53341a52b8e
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-03-08 18:17:20 +1100

    finished test

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, "");
+  }
+
+  static titleize(sentence) {
+    const exclusions = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    return sentence
+      .split(" ")
+      .map((word, index) => {
+        if (index === 0 || !exclusions.includes(word)) {
+          return this.capitalize(word);
+        } else {
+          return word;
+        }
+      })
+      .join(" ");
+  }
 }
